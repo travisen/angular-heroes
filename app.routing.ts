eb19033e4b60089015dfa770cbfc4299ea0dc94e
travisen <travis.nykaza@gmail.com>
2016-08-23 14:39:19 -0400

    Add part 6

---
+++ app.routing.ts
@@ -0,0 +1,11 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HeroesComponent } from './heroes.component';
+
+const appRoutes: Routes = [
+	{
+		path: 'heroes',
+		component: HeroesComponent
+	}
+];
+export const routing = RouterModule.forRoot(appRoutes);
